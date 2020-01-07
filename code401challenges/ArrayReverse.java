58794a573bdea9b3482ca2e9204d35671dc9ef2a
Sergey Voytov <sergvoytov@gmail.com>
2020-01-07 14:05:06 -0800

    working code

--- code401challenges/ArrayReverse.java
+++ code401challenges/ArrayReverse.java
@@ -1,4 +1,4 @@
-..import java.util.Arrays;
+import java.util.Arrays;
 public class ArrayReverse {
   public static void main (String[] args) {
     // create the array to test your code on later
@@ -12,7 +12,6 @@ public class ArrayReverse {
   }
   // the method you should write, to reverse an array
   public static int[] reverseArray(int[] inputArray) {
-
     int[] reverseArray = new int[inputArray.length];
     int i = 0;
     int length = inputArray.length - 1;
