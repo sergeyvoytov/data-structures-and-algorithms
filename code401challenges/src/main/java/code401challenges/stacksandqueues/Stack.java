580a3d786db5b20994d0330066751c882d5607c9
Sergey Voytov <sergvoytov@gmail.com>
2020-01-21 09:29:04 -0800

    work in progress

--- code401challenges/src/main/java/code401challenges/stacksandqueues/Stack.java
+++ code401challenges/src/main/java/code401challenges/stacksandqueues/Stack.java
@@ -1,9 +1,13 @@
 package code401challenges.stacksandqueues;
 
 
-public class Stack {
+import java.util.EmptyStackException;
 
-    Node top;
+public class Stack<Potato> {
+
+    Potato russet;
+
+    Node<Potato> top;
 
 //    Create a Stack class that has a top property. It creates an empty Stack when instantiated.
 //    This object should be aware of a default empty value assigned to top when the stack is created.
@@ -13,28 +17,35 @@ public class Stack {
     }
 
     //       takes any value as an argument and adds a new node with that value to the top of the stack
-    public void push(int value) {
-        Node newNode = new Node(value, this.top);
+    public void push(Potato value) {
+        Node<Potato> newNode = new Node<>(value, this.top);
         this.top = newNode;
     }
 
 
     //   removes the node from the top of the stack, and returns the node’s value.
-    public int pop() {
+    public Potato pop() {
 
         if (this.top == null) {
             System.out.println("stack is empty");
         } else {
-            Node temp = top;
+            Node<Potato> temp = top;
             top = top.next;
             return temp.value;
         }
-        return -1;
+        throw new EmptyStackException();
+
     }
 
     //    returns the value of the node located on top of the stack, without removing it from the stack.
-    public int peek() {
-        return this.top.value;
+    public Potato peek() {
+        if (!isEmpty()) {
+            return this.top.value;
+        } else {
+
+            System.out.println("stack is empty");
+            throw new EmptyStackException();
+        }
     }
 
 
